6e272f4c68a7a9a5361766d88f65fcf2143e2c5b
cnbc4 <cnbc4@DESKTOP-2VG1U49.dhcp.wustl.edu>
2023-04-06 12:46:35 -0500

    burger

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -2,9 +2,33 @@ package studio8;
 
 public class Date {
 
+	
+	private int day;
+	private int month;
+	private int year;
+	private boolean holiday;
+	
+	public Date (int day, int month, int year, boolean holiday) {
+		this.day=day;
+		this.month=month;
+		this.year=year;
+		this.holiday=holiday;
+	}
+	
+	
+	
+	public String toString() {
+		return "Date: "+month+"/"+day+"/"+year;
+	}
 
-    public static void main(String[] args) {
 
+
+
+	public static void main(String[] args) {
+    	
+    	
+    	
+    	
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,10 +2,42 @@ package studio8;
 
 
 public class Time {
+	
+	private int hour;
+	private int minute;
+	private boolean is24Hour;
+	
+	public Time (int hour, int minute, boolean is24Hour) {
+		this.hour=hour;
+		this.minute=minute;
+		this.is24Hour=is24Hour;
+	}
+	
+	public String toString() {
+		if (is24Hour == true) {
+			return hour+12+":"+minute;
+		}
+		else {
+			return hour+":"+minute;
+		}
+	}
 
 	public static void main(String[] args) {
- 
-    	
+		Time t1= new Time (6, 23, true);
+    	System.out.println(t1);
+    	Time t2= new Time (18, 23, false);
+    	System.out.println(t1.equals(t2));
     }
+	
+	public boolean equals (Time t1) {
+		int hour2 = 0;
+		if (is24Hour == true) {
+			hour2 = hour-12;
+			if (hour2 == t1.hour) {
+				
+			}
+		}
+		if
+	}
 
 }
